21562e73d04a172f6b5aa7b361522970cd2b6d70
GOST1706 <jbugra23@outlook.es>
2026-02-06 20:16:17 -0500

    Nuevo parche

--- main.py
+++ main.py
@@ -3,5 +3,6 @@ import sys
 
 if __name__ == '__main__':
     rango = int(sys.argv[1])
+    if not rango: rango = 0
     print("Generando un numero aleatorio en un rango de 0 a " + str(rango))
     print(random.choice(range(rango)))
